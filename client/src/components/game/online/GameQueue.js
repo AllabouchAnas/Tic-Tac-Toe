5d5666bbbc6119b2fc3baecdbcc6538aa1ee0bf2
AllabouchAnas <allabouch2004@gmail.com>
2024-03-08 19:33:44 +0100

    SocketIO rooms logic setup

--- client/src/components/game/online/GameQueue.js
+++ client/src/components/game/online/GameQueue.js
@@ -1,26 +1,41 @@
-import React, { useState } from 'react';
-import { Navigate  } from 'react-router-dom';
-
+import React, { useState, useEffect, useRef } from 'react';
+import GameOnline from './GameOnline'; // Import the GameOnline component
+import io from 'socket.io-client';
 
 function GameQueue() {
-  const [playersJoined, setPlayersJoined] = useState(0);
+    const [playersJoined, setPlayersJoined] = useState(0);
+    const [roomName, setRoomName] = useState('');
+    const socket = useRef(null);
 
-  // Function to handle when a player joins
-  const handlePlayerJoin = () => {
-    setPlayersJoined(playersJoined + 1);
-  };
+    useEffect(() => {
+        // Connect to Socket.IO server
+        socket.current = io();
 
-  return (
-    <div className='game-queue'>
-      {playersJoined < 2 ? (
-        <div>
-          <p>Waiting for another player to join ...</p>
-        </div>
-      ) : (
-        <Navigate to="/gameonline" />
-      )}
-    </div>
-  );
+        // Join the queue when the component mounts
+        socket.current.emit('joinQueue');
+
+        // Listen for start game event and receive the room name
+        socket.current.on('startGame', (room) => {
+            setRoomName(room);
+            setPlayersJoined(2); // Set playersJoined to 2 to trigger the redirection
+        });
+
+        return () => {
+            // Disconnect from Socket.IO server when component unmounts
+            socket.current.disconnect();
+        };
+    }, []);
+
+    // Render the GameOnline component if enough players have joined and a room name is available
+    if (playersJoined >= 2 && roomName) {
+        return <GameOnline room={roomName} />;
+    } else {
+        return (
+            <div className='game-queue'>
+                <p>Waiting for another player to join ...</p>
+            </div>
+        );
+    }
 }
 
 export default GameQueue;
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 require('dotenv').config();
-const UserRoutes = require('./routes/UserRoutes');
+const UserRoutes = require('./routes/UserRoutes')
 const { argv } = require('process');
 
 // Express App
@@ -17,18 +17,69 @@ const io = new Server(httpServer, {
   /* options */
 });
 
+const MAX_USERS_PER_ROOM = 2; // Maximum number of users per room
+
+// Map to track players waiting to join a game
+const queue = new Map(); 
+
 io.on("connection", (socket) => {
-  console.log("A user connected");
+    console.log(`User connected with ID: ${socket.id}`);
 
-  socket.on("num", (arg) => {
-    // io.emit('num', arg); // Emit data to all connected clients
-    socket.broadcast.emit('num', arg);
+    // Handle player joining the queue
+    socket.on("joinQueue", () => {
+        queue.set(socket.id, socket); // Add player to the queue
+
+        // Check if there are enough players in the queue to start a game
+        if (queue.size >= MAX_USERS_PER_ROOM) {
+            // Match two players and create a room for them
+            const roomName = 'room_' + Math.random().toString(36).substring(7); // Generate a unique room name
+            const players = Array.from(queue.values()).splice(0, 2); // Get first two players from the queue
+
+            players.forEach(player => {
+                player.join(roomName);
+                queue.delete(player.id); // Remove player from the queue
+                console.log(`User: ${socket.id} connected to room: ${roomName}`)
+            });
+
+            // Notify players to start the game and pass the room name
+            io.to(roomName).emit('startGame', roomName);
+        }
+    });
+
+    socket.on("joinRoom", (room) => {
+        const roomClients = io.sockets.adapter.rooms.get(room); // Get all clients in the room
+        // if (roomClients && roomClients.size >= MAX_USERS_PER_ROOM) {
+        //     // Room is full, do not allow more users to join
+        //     socket.emit("roomFull");
+        //     return;
+        // }
+
+        socket.join(room);
+        console.log(`User ${socket.id} joined room ${room}`)
+    });
+
+    socket.on("num", (arg) => {
+      socket.to(arg.room).emit('num', arg); // Emit data to all clients in the same room except the sender
+      console.log(arg)
   });
 
-  // Handle events here
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-  });
+    // Handle events here
+    socket.on("disconnect", () => {
+        console.log(`User disconnected with ID: ${socket.id}`);
+        // Remove the disconnected user from the queue if they were in the queue
+        if (queue.has(socket.id)) {
+            queue.delete(socket.id);
+        }
+        // Update the count of users in the room upon disconnection
+        const rooms = io.sockets.adapter.rooms;
+        if (rooms) {
+            rooms.forEach((value, room) => {
+                if (value.has(socket.id)) {
+                    io.to(room).emit('userLeft', socket.id);
+                }
+            });
+        }
+    });
 });
 
 
